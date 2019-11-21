90c9c705b0d0d94eeab18408d4069d44b37c5272
huya1121 <huya1121@users.noreply.github.com>
2019-11-21 22:43:00 +0800

    Create v2ray.sh

---
+++ v2ray.sh
@@ -0,0 +1,58 @@
+#/bin/bash
+
+ngx_php(){
+apt-get update -y && apt-get upgrade -y
+apt-get install nginx wget socat php7.0-fpm php7.0-curl php7.0-zip php7.0-mysql php7.0-mbstring php7.0-xml php7.0-gd -y
+sed -i "s/\;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.0/fpm/php.ini
+/etc/init.d/php7.0-fpm restart
+}
+
+install_acme(){
+if [ ! -d "/root/.acme.sh" ]; then
+echo "安装acme.sh"
+curl  https://get.acme.sh | sh
+echo "alias acme.sh=~/.acme.sh/acme.sh" >> /root/.bashrc
+source /root/.bashrc
+echo "acme.h 安装完成,生成证书"
+fi
+}
+acme_cer(){
+systemctl stop nginx
+acme.sh  --issue -d $domain  --standalone --force
+systemctl start nginx
+echo "证书生成完成！"
+}
+
+v2ray(){
+echo "开始安装v2ray"
+ouid=sed -n '25p' /etc/v2ray/config.json | awk -F'"' '{print $4}'
+uid=`cat /proc/sys/kernel/random/uuid`
+bash <(curl -L -s https://install.direct/go.sh)
+wget -qO  /etc/v2ray/config.json  https://raw.githubusercontent.com/huya1121/v2-shell/master/config.json
+sed -i "s/$ouid/$uid/g" /etc/v2ray/config.json
+systemctl restart v2ray
+echo "v2ray 安装完成！"
+}
+
+conf_nginx(){
+wget -qO /etc/nginx/sites-available/ https://raw.githubusercontent.com/huya1121/v2-shell/master/v2.conf
+ln -s /etc/nginx/sites-available/v2.conf /etc/nginx/sites-enable/
+sed 's/$domain/abc.com/g' /etc/nginx/sites-available/v2.conf
+/etc/init.d/nginx restart
+}
+
+
+
+read -p "Please input your domain name:" domain
+echo "您输入的域名是：$domian"
+ngx_php
+acme
+acme_cer
+v2ray
+conf_nginx
+echo "服务器: $domain"
+echo "端口：443"
+echo "UUID：$uid"
+echo "PATH：/api/"
+echo "WS+TLS"
+echo "install complete"
