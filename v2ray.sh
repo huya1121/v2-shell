85a84882dff5a46f995597df082891d2dfdc87ce
huya1121 <huya1121@users.noreply.github.com>
2019-11-21 23:39:08 +0800

    Update v2ray.sh
    显示优化~

--- v2ray.sh
+++ v2ray.sh
@@ -27,16 +27,22 @@ echo "证书生成完成！"
 v2ray(){
 echo "开始安装/更新v2ray"
 bash <(curl -L -s https://install.direct/go.sh)
-wget -qO  /etc/v2ray/config.json  https://raw.githubusercontent.com/huya1121/v2-shell/master/config.json
 systemctl restart v2ray
 echo "v2ray 安装完成！"
 }
 
 change_v2conf(){
+wget -qO  /etc/v2ray/config.json  https://raw.githubusercontent.com/huya1121/v2-shell/master/config.json
 ouid=`sed -n '25p' /etc/v2ray/config.json | awk -F'"' '{print $4}'`
 uid=`cat /proc/sys/kernel/random/uuid`
 sed -i "s/$ouid/$uid/g" /etc/v2ray/config.json
-echo "UUID=$uid"
+}
+
+conf_nginx(){
+wget -qO /etc/nginx/sites-available/v2.conf https://raw.githubusercontent.com/huya1121/v2-shell/master/v2.conf
+ln -s /etc/nginx/sites-available/v2.conf /etc/nginx/sites-enabled/v2.conf
+sed -i 's/abc.com/$domain/g' /etc/nginx/sites-available/v2.conf
+/etc/init.d/nginx restart
 }
 
 v2_info(){
@@ -47,15 +53,6 @@ echo "PATH：/api/"
 echo "WS+TLS"
 echo "安装完成"
 }
-
-conf_nginx(){
-wget -qO /etc/nginx/sites-available/v2.conf https://raw.githubusercontent.com/huya1121/v2-shell/master/v2.conf
-ln -s /etc/nginx/sites-available/v2.conf /etc/nginx/sites-enabled/v2.conf
-sed -i 's/abc.com/$domain/g' /etc/nginx/sites-available/v2.conf
-/etc/init.d/nginx restart
-}
-
-
 
 read -p "请输入域名:" domain
 echo "您输入的域名是：$domain"
