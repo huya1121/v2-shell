9662995673a1b2358badd06c1bce4e27a5316200
huya1121 <huya1121@users.noreply.github.com>
2020-01-18 19:30:46 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 depend(){
 apt-get update -y && apt-get upgrade -y
-apt-get install wget socat curl -y
+apt-get install wget socat curl zip -y
 read -p "请输入域名:" domain
 echo "您输入的域名是：$domain"
 }
