b949a4c50c725ce03493fbde6ecea037ac2be9f1
huya1121 <huya1121@users.noreply.github.com>
2021-07-16 07:55:41 +0800

    Create ss-obfs.sh

---
+++ ss-obfs.sh
@@ -0,0 +1,48 @@
+#/bin/bash
+sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
+apt update
+apt -t stretch-backports install shadowsocks-libev simple-obfs -y
+apt-get install libsodium-dev -y
+apt-get clean
+
+read -p "请输入SS端口:" SSPORT
+read -p "请输入SS密码:" SSPASSWORD
+
+cat > /etc/shadowsocks-libev/config.json << EOF
+{
+    "server":"0.0.0.0",
+    "server_port":$SSPORT,
+    "password":"$SSPASSWORD",
+    "timeout":300,
+    "user":"nobody",
+    "method":"chacha20-ietf-poly1305",
+    "fast_open":false,
+    "nameserver":"1.0.0.1",
+    "mode":"tcp_and_udp",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=http"
+}
+EOF
+
+cat > /etc/systemd/system/shadowsocks.service << EOF
+[Unit]
+Description=shadowsocks-libev
+After=network.target
+[Service]
+Type=simple
+User="nobody"
+ExecStart=/usr/bin/ss-server -c /etc/shadowsocks-libev/config.json
+Restart=always
+LimitNOFILE=512000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl disable shadowsocks-libev
+systemctl stop shadowsocks-libev
+systemctl enable shadowsocks
+systemctl restart shadowsocks
+echo "SS密码为$SSPORT"
+echo "SS端口为$SSPASSWORD"
+echo "SS加密方式为chacha20-ietf-poly1305"
+echo "obfs混淆为http"
