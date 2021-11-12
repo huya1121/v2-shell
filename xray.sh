1901f659a6182d8f1c3bfd687d7b8f10a3f0244b
huya1121 <huya1121@users.noreply.github.com>
2021-11-12 11:29:32 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -32,13 +32,14 @@ else
 echo "安装证书，请检查配置或者重新安装！"
 exit 2
 fi
-systemctl start nginx
+
+systemctl start nginx || /etc/init.d/nginx start
+
 mkdir /etc/nginx/ssl
 /root/.acme.sh/acme.sh --install-cert -d $domain \
 --key-file       /etc/nginx/ssl/$domain.key.pem  \
 --fullchain-file /etc/nginx//ssl/$domain.cert.pem \
 --reloadcmd     "service nginx force-reload"
-systemctl start nginx || /etc/init.d/nginx start
 
 chown nobody /etc/nginx/ssl/$domain.key.pem
 chown nobody /etc/nginx//ssl/$domain.cert.pem
