8749835e4fd889d9facebe109d6a200da98f6bd1
huya1121 <huya1121@users.noreply.github.com>
2021-11-20 12:40:03 +0800

    Update openvpnas-cert.sh

--- openvpnas-cert.sh
+++ openvpnas-cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-/usr/local/openvpn_as/scripts/confdba -mk cs.ca_bundle -v "`cat /etc/nginx/ssl/$domain.cert.pem`"
-/usr/local/openvpn_as/scripts/confdba -mk cs.priv_key -v "`cat /etc/nginx/ssl/$domain.key.pem`"
-/usr/local/openvpn_as/scripts/confdba -mk cs.cert -v "`cat cat /etc/nginx/ssl/$domain.cert.pem`"
+/usr/local/openvpn_as/scripts/confdba -mk cs.ca_bundle -v "`cat /etc/nginx/ssl/abc.com.cert.pem`"
+/usr/local/openvpn_as/scripts/confdba -mk cs.priv_key -v "`cat /etc/nginx/ssl/abc.com.key.pem`"
+/usr/local/openvpn_as/scripts/confdba -mk cs.cert -v "`cat cat /etc/nginx/ssl/abc.com.cert.pem`"
 /bin/sleep 1
