e7edbcda57e30f516ee08294db6b307642811053
huya1121 <huya1121@users.noreply.github.com>
2021-11-20 12:12:49 +0800

    Create openvpn.sh

---
+++ openvpn.sh
@@ -0,0 +1,49 @@
+#install openvpn-as
+ 
+apt update && apt -y install ca-certificates wget net-tools gnupg nginx
+curl  https://get.acme.sh | sh -s email=my@example.com
+
+read -p "请输入域名:" domain
+
+acme(){
+echo "安装证书中"
+/etc/init.d/nginx stop
+/root/.acme.sh/acme.sh --issue -d $domain   --standalone
+/etc/init.d/nginx start
+mkdir -p /etc/nginx/ssl/
+acme.sh --install-cert -d $domain \
+--key-file       /etc/nginx/ssl/$domain.key.pem  \
+--fullchain-file /etc/nginx/ssl/$domain.cert.pem \
+--reloadcmd     "service nginx force-reload"
+if [ $? eq 0 ]; then {
+echo "安装证书完成"
+}
+else {
+echo "安装证书失败"
+}
+fi
+}
+
+
+openvpnas() {
+wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
+echo "deb http://as-repository.openvpn.net/as/debian buster main">/etc/apt/sources.list.d/openvpn-as-repo.list
+apt update && apt -y install openvpn-as
+mkdir -p /etc/systemd/system/openvpnas.service.d
+cat > /etc/systemd/system/openvpnas.service.d/override.conf <<EOF
+[Service]
+ExecStartPre=/opt/openvpnas-cert.sh
+EOF
+
+cat > /opt/openvpnas-cert.sh <<EOF
+#!/bin/bash
+set -x
+/usr/local/openvpn_as/scripts/confdba -mk cs.ca_bundle -v "`cat /etc/nginx/ssl/$domain.cert.pem`"
+/usr/local/openvpn_as/scripts/confdba -mk cs.priv_key -v "`cat /etc/nginx/ssl/$domain.key.pem`"
+/usr/local/openvpn_as/scripts/confdba -mk cs.cert -v "`cat cat /etc/nginx/ssl/$domain.cert.pem`"
+/bin/sleep 1
+EOF
+}
+
+acme
+openvpnas
