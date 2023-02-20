f9ca9e7a3f9f08ecfe431ff4417161a5a3142d91
huya1121 <huya1121@users.noreply.github.com>
2023-02-20 21:30:30 +0800

    Update and rename ss-obfs.sh to ss.sh

--- ss.sh
+++ ss.sh
@@ -1,7 +1,7 @@
 #Debian10 supported
 #/bin/bash
 apt update
-apt-get install libsodium-dev shadowsocks-libev simple-obfs -y
+apt-get install libsodium-dev shadowsocks-libev -y
 apt-get clean
 
 read -p "请输入SS端口:" SSPORT
@@ -13,12 +13,10 @@ cat > /etc/shadowsocks-libev/config.json << EOF
     "server_port":$SSPORT,
     "password":"$SSPASSWORD",
     "timeout":300,
-    "method":"chacha20-ietf-poly1305",
+    "method":"aes-256-gcm",
     "fast_open":false,
     "nameserver":"1.0.0.1",
     "mode":"tcp_and_udp",
-    "plugin":"obfs-server",
-    "plugin_opts":"obfs=http"
 }
 EOF
 
@@ -41,5 +39,4 @@ systemctl enable shadowsocks
 systemctl restart shadowsocks
 echo "SS密码为$SSPORT"
 echo "SS端口为$SSPASSWORD"
-echo "SS加密方式为chacha20-ietf-poly1305"
-echo "obfs混淆为http"
+echo "SS加密方式为aes-256-gcm"
